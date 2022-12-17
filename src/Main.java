80a984622bcbcd49cbc31f33a6a41f35f828ae7f
IrinaMehnetsova <i7417m@gmail.com>
2022-12-17 23:03:39 +0300

    Initiai commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 20_000;
+        int bonus = 20;
+
+
+        int result = ticketPrice / bonus;
+
+        System.out.println(result);
+
+
+    }
+
+}
